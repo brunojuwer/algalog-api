7893813c0e051c807be81c46bc0cac41199dbca2
Bruno Juwer <bruno.juwer1997@gmail.com>
2022-09-16 16:52:03 -0300

    Added Cliente Repository

--- src/main/java/br/com/juwer/algalog/api/controller/ClienteController.java
+++ src/main/java/br/com/juwer/algalog/api/controller/ClienteController.java
@@ -2,23 +2,40 @@ package br.com.juwer.algalog.api.controller;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.juwer.algalog.domain.model.Cliente;
+import br.com.juwer.algalog.domain.repository.ClienteRepository;
 
 @RestController
+@RequestMapping("/clientes")
 public class ClienteController {
 
-  @PersistenceContext
-  private EntityManager manager;
+  @Autowired
+  private ClienteRepository clienteRepository;
 
-  @GetMapping("/clientes")
+  @GetMapping
   public List<Cliente> listar(){
-    return manager.createQuery("from Cliente", Cliente.class)
-              .getResultList(); 
+    return clienteRepository.findAll(); 
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Cliente>  listarPorId(@PathVariable Long id){
+      return clienteRepository.findById(id)
+              .map(ResponseEntity::ok)
+              .orElse(ResponseEntity.notFound().build());
+
+      // Optional<Cliente> cliente = clienteRepository.findById(id);
+
+      // if(cliente.isPresent()){
+      //   return ResponseEntity.ok(cliente.get());
+      // }
+
+      // return ResponseEntity.notFound().build();
   }
 }
