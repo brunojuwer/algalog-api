19ec8917faedb64d178e5d3a636f6ee685a53ac2
Bruno Juwer <bruno.juwer1997@gmail.com>
2022-09-17 22:51:11 -0300

    Added Bean Validation at Client class

--- src/main/java/br/com/juwer/algalog/api/controller/ClienteController.java
+++ src/main/java/br/com/juwer/algalog/api/controller/ClienteController.java
@@ -2,6 +2,8 @@ package br.com.juwer.algalog.api.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -47,13 +49,13 @@ public class ClienteController {
 
   @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
-  public Cliente adicionar(@RequestBody Cliente cliente){
+  public Cliente adicionar(@Valid @RequestBody Cliente cliente){
       return clienteRepository.save(cliente);
   }
 
   @PutMapping("/{id}")
   public ResponseEntity<Cliente> atualizar(@PathVariable Long id, 
-    @RequestBody Cliente cliente){
+    @Valid @RequestBody Cliente cliente){
       
       if(!clienteRepository.existsById(id)){
         return ResponseEntity.notFound().build();
