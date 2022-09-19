0e77b4bc94c2e5be870bca7bab70a37d75190b75
Bruno Juwer <bruno.juwer1997@gmail.com>
2022-09-19 15:40:30 -0300

    Created representation models

---
+++ src/main/java/br/com/juwer/algalog/api/model/EntregaModel.java
@@ -0,0 +1,21 @@
+package br.com.juwer.algalog.api.model;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
+import br.com.juwer.algalog.domain.model.StatusEntrega;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EntregaModel {
+  
+  private Long Id;
+  private String nomeCliente;
+  private DestinatarioModel destinatario;
+  private BigDecimal taxa;
+  private StatusEntrega status;
+  private OffsetDateTime dataPedido;
+  private OffsetDateTime dataFinalizacao; 
+}
